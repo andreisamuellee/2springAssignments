66197f6926a3517c7de6251973ee1b9b45a832d5
andreisamuellee <54439182+andreisamuellee@users.noreply.github.com>
2021-01-23 12:19:53 +0200

    done

--- src/index.js
+++ src/index.js
@@ -1,146 +1,66 @@
-let lowestNum = 1;
-let highestNum = 100;
 
-let randomNumber = Math.floor(Math.random() * highestNum) + lowestNum;
-const guesses = document.querySelector('.guesses');
-const lastResult = document.querySelector('.lastResult');
-const lowOrHi = document.querySelector('.lowOrHi');
-const guessSubmit = document.querySelector('.guessSubmit');
-const guessField = document.querySelector('.guessField');
-let guessCount = 1;
-let resetButton;
-let maxGuessCount = 10;
-let startTime;
-let endTime;
+import FazerMenu from './assets/fazer-week-example.json';
+console.log('FazerMenu', FazerMenu);
 
-const info = document.querySelector('.info');
-const text = document.createTextNode("We have selected a random number between " + lowestNum + " and " + highestNum + ". See if you can guess it in " + maxGuessCount + " turns or fewer. We'll tell you if your guess was too high or too low.");
-info.appendChild(text);
+const menu = [
+    {name: 'Lingonberry jam', price: 4.00},
+    {name: 'Mushroom and bean casserole', price: 5.50},
+    {name: 'Chili-flavoured wheat', price: 3.00},
+    {name: 'Vegetarian soup', price: 4.80},
+    {name: 'Pureed root vegetable soup with smoked cheese', price: 8.00}
+  ];
 
-function checkGuess() {
-  let userGuess = Number(guessField.value);
-  if (guessCount === 1) {
-    guesses.textContent = 'Previous guesses: ';
-    startTime = Date.now();
-  }
-
-  guesses.textContent += userGuess + ' ';
-
-  if (userGuess === randomNumber) {
-    endTime = Date.now();
-    lastResult.textContent = 'Congratulations! You got it right! Your time: ' + ((endTime - startTime) / 1000) + ' seconds';
-    lastResult.style.backgroundColor = 'green';
-    lowOrHi.textContent = '';
-    setGameOver();
-  } else if (guessCount === maxGuessCount) {
-    lastResult.textContent = '!!!GAME OVER!!!';
-    lowOrHi.textContent = '';
-    setGameOver();
-  } else {
-    lastResult.textContent = 'Wrong!';
-    lastResult.style.backgroundColor = 'red';
-    if (userGuess < randomNumber) {
-      lowOrHi.textContent = 'Last guess was too low!';
-    } else if (userGuess > randomNumber) {
-      lowOrHi.textContent = 'Last guess was too high!';
-    }
-  }
-
-  guessCount++;
-  guessField.value = '';
-  guessField.focus();
-}
-
-guessSubmit.addEventListener('click', checkGuess);
-
-function setGameOver() {
-  guessField.disabled = true;
-  guessSubmit.disabled = true;
-  resetButton = document.createElement('button');
-  resetButton.textContent = 'Start new game';
-  document.body.appendChild(resetButton);
-  resetButton.addEventListener('click', resetGame);
-}
-
-function resetGame() {
-  guessCount = 1;
-  const resetParas = document.querySelectorAll('.resultParas p');
-  for (let i = 0; i < resetParas.length; i++) {
-    resetParas[i].textContent = '';
-  }
-
-  resetButton.parentNode.removeChild(resetButton);
-  guessField.disabled = false;
-  guessSubmit.disabled = false;
-  guessField.value = '';
-  guessField.focus();
-  lastResult.style.backgroundColor = 'white';
-  randomNumber = Math.floor(Math.random() * highestNum) + lowestNum;
-}
-
-
-//code always gets the average of last guess and highest/lowest possible number. This way the code drops half of guesses needed on every guess.
-
-//9 guesses max.
-//1 guess min.
-const play = () => {
-  let firstGuess = highestNum / 2;
-  let lastGuess = firstGuess;
-  let lowest = lowestNum;
-  let highest = highestNum;
-  let guess = 10;
-  let win = false;
-  guessField.value = firstGuess;
-  guessSubmit.click();
-  let guessNum = 0;
-
-  while (!win) {
-    if (lowOrHi.innerText === 'Last guess was too low!') {
-      guess = Math.round((lastGuess + highest) / 2);
-      guessField.value = guess;
-      lowest = lastGuess;
-      lastGuess = guess;
-      guessSubmit.click();
-      guessNum++;
-    }
-    else if (lowOrHi.innerText === 'Last guess was too high!') {
-      guess = Math.round((lastGuess + lowest) / 2);
-      guessField.value = guess;
-      highest = lastGuess;
-      lastGuess = guess;
-      guessSubmit.click();
-      guessNum++;
-    }
-    else {
-      win = true;
-      resetGame();
-    }
-  }
-  return guessNum;
+const validateName = (mealName) => {
+  const nameVal = /^[A-ZÅÄÖ]{1}[a-zåäöA-ZÅÄÖ0-9\-\ \/,()]{3,63}$/;
+  return nameVal.test(mealName);
 };
 
-let games = 0;
+console.log('mealname validated:', validateName('Lingonberry jam'));
 
-//total number of guesses per game.
-const guessesPerGame = [];
-
-//runs game 1000 times
-while (games < 1000) {
-  guessesPerGame.push(play());
-  games++;
-};
-console.log(guessesPerGame);
-
-
-console.log('games '+games);
-
-
-//calculates average. (~4.84 guesses per game)
-let total = 0;
-for(let i = 0; i < guessesPerGame.length; i++) {
-    total += guessesPerGame[i];
+for (const item of Object.values(menu)) {
+  console.log('mealname ' + item.name + ' validated:', validateName(item.name));
 }
-let avg = total / guessesPerGame.length;
 
+const sortMenu = (menu) => {
+  const sorted = menu.sort((a, b) => a.price - b.price);
+  return sorted;
+};
+console.log('sorted menu', sortMenu(menu));
 
-console.log('Average guesses per game = '+avg);
+const filterByPrice = (menu, priceLimit) => {
+  return menu.filter(item => item.price < priceLimit);
+};
+console.log('filtered menu', filterByPrice(menu, 5));
+
+const raisePrices = (menu, percent) => {
+  return menu.map(item => {
+    return {
+      name: item.name,
+      price: item.price * (1 + percent / 100)
+    };
+  });
+};
+console.log('price raised:', raisePrices(menu, 15));
+
+const totalPrice = (menu) => {
+  return menu.reduce((a, b) => {
+    return {price: a.price + b.price};
+  });
+};
+console.log('total price', totalPrice(menu));
+
+const vegOnly = (menuData) => {
+  let vegMeals = [];
+  console.log(menuData.LunchMenus[0].SetMenus);
+  for (const setMenu of menuData.LunchMenus[0].SetMenus) {
+    console.log(setMenu);
+    for (const meal of setMenu.Meals) {
+      if (meal.Diets.includes('Veg')) {
+        vegMeals.push(meal.Name);
+      }
+    }
+  }
+  return vegeMeals;
+};
+console.log('test vegan meal function', vegOnly(FazerMenu));
+
