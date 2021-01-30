abeb6845ca7bb3fda68634e874135ac21367f309
andreisamuellee <54439182+andreisamuellee@users.noreply.github.com>
2021-01-30 16:42:04 +0200

    done

--- src/index.js
+++ src/index.js
@@ -29,6 +29,16 @@ let SodexoSortEn = false;
 
 SodexoCoursesFi.forEach(SodexoMyFunction);
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('./service-worker.js').then(registration => {
+      console.log('SW registered: ', registration);
+    }).catch(registrationError => {
+      console.log('SW registration failed: ', registrationError);
+    });
+  });
+}
+
 function SodexoDisplayFood() {
   if (SodexoLang) {
     list.innerHTML = '';
--- webpack.common.js
+++ webpack.common.js
@@ -3,14 +3,35 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 const WriteFilePlugin = require('write-file-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
 
 module.exports = {
   entry: {
     app: './src/index.js',
   },
   plugins: [
+    new WebpackPwaManifest({
+      name: 'Lunch Progressive Web App',
+      short_name: 'LunchPWA',
+      description: 'Describe your Progressive Web App here',
+      background_color: '#ffffff',
+      crossorigin: 'use-credentials',
+      icons: [
+        {
+          src: path.resolve('src/assets/icon.png'),
+          sizes: [96, 128, 192, 256, 384, 512]
+        },
+      ]
+    }),
     new CleanWebpackPlugin(),
     new WriteFilePlugin(),
+    new WorkboxPlugin.GenerateSW({
+      // these options encourage the ServiceWorkers to get in there fast
+      // and not allow any straggling "old" SWs to hang around
+      clientsClaim: true,
+      skipWaiting: true,
+    }),
     new CopyPlugin({
       patterns: [
       {
@@ -57,5 +78,5 @@ module.exports = {
         ]
       }
     ]
-  }
+  },
 };
