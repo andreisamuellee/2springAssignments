41f545a4c803ca61de7a977e6ca9d01c86bbb9ae
andreisamuellee <54439182+andreisamuellee@users.noreply.github.com>
2021-01-30 15:44:03 +0200

    done

--- src/modules/fazer-data.js
+++ src/modules/fazer-data.js
@@ -7,18 +7,18 @@ console.log('fazer lunch menu object en ', LunchMenuEn);
 const coursesEn = [];
 const coursesFi = [];
 
-activate();
+activate(0);
 
-function activate() {
-  for (let course in LunchMenuFi.LunchMenus[0].SetMenus) {
-    let data = LunchMenuFi.LunchMenus[0].SetMenus[course];
+function activate(dayOfWeek) {
+  for (let course in LunchMenuFi.LunchMenus[dayOfWeek].SetMenus) {
+    let data = LunchMenuFi.LunchMenus[dayOfWeek].SetMenus[course];
     for (let meal in data.Meals) {
       let food = data.Meals[meal];
       coursesFi.push(food.Name);
     }
   }
-  for (let course in LunchMenuEn.LunchMenus[0].SetMenus) {
-    let data = LunchMenuEn.LunchMenus[0].SetMenus[course];
+  for (let course in LunchMenuEn.LunchMenus[dayOfWeek].SetMenus) {
+    let data = LunchMenuEn.LunchMenus[dayOfWeek].SetMenus[course];
     for (let meal in data.Meals) {
       let food = data.Meals[meal];
       coursesEn.push(food.Name);
